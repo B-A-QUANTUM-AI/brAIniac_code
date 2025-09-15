1cadb04befead1c1c11cf63a6d29a2c928d42aa4
Chidicivok <chidicivok@gmail.com>
2025-09-15 14:19:01 +0200

    Update brAIniac.py

--- brAIniac.py
+++ brAIniac.py
@@ -1,24 +1,3 @@
-# brAIniac code base
-# for file management and interaction with my OS
-import os
-
-# for data analyisis
-import pandas as pd
-
-BASE_DIRECTORY = "C:/Users/chidi/Downloads/brisc2025/classification_task"
-
-TRAIN_DIRECTORY = os.path.join(BASE_DIRECTORY, "train")
-TEST_DIRECTORY = os.path.join(BASE_DIRECTORY, "test")
-
-
-IMAGE_SIZE = (224,224)
-
-BATCH_SIZE = 32
-
-# better handling of path files
-from pathlib import Path
-
-# CLASS_NAMES = sorted([d.name for d in Path(TRAIN_DIRECTORY).iterdir() if d.is_dir()])
-# print (CLASS_NAMES)
-# remeber the index shows the class 0, 1, 2, 3
-CLASS_NAMES = ["glioma", "meningioma", "no_tumor", "pituitary"]
+# leave the first 2 lines of code here 
+from keras.models import Sequential
+cnn_model= Sequential()
